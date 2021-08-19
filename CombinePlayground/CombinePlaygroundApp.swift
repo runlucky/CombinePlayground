f2c35bcbed6755abbc05be7406ef715716488c02
Kakeru <stand.by.we@gmail.com>
2021-08-19 17:59:56 +0900

    ガワ

---
+++ CombinePlayground/CombinePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePlaygroundApp.swift
+//  CombinePlayground
+//
+//  Created by H6244 on 2021/08/19.
+//
+
+import SwiftUI
+
+@main
+struct CombinePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombinePlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombinePlayground
+//
+//  Created by H6244 on 2021/08/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
